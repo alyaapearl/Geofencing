83d160fa93adc1cf208877749b79773ae23b0a97
alyaapearl <alyaapearl@gmail.com>
2018-02-01 09:25:22 +0800

    Delete client2.py

--- client2.py
+++
@@ -1,9 +0,0 @@
-import socket
-client_socket = socket.socket()
-server_address = 'localhost'
-server_port = 3190
-client_socket.connect((server_address, server_port))
-data = client_socket.recv(1024)
-print data
-
-client_socket.close()
